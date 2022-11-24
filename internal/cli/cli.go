825762e4d877745aa4fc6beb8100769941c7c0d7
RomanShirov <RomanShirov.dev@yandex.ru>
2022-11-24 18:05:02 +0300

    Add config loading mode select

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -1,1 +1,22 @@
 package cli
+
+import (
+	"fmt"
+	"github.com/manifoldco/promptui"
+)
+
+func SelectLoadConfigMode() (string, error) {
+	prompt := promptui.Select{
+		Label: "Choose a load configuration mode",
+		Items: []string{"From .env configuration file", "Select manually"},
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Incorrect input error: %v\n", err)
+		return "", err
+	}
+
+	return result, nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"github.com/RomanShirov/loadtesting-cli/internal/cli"
+	"log"
+)
 
+func main() {
+	_, err := cli.SelectLoadConfigMode()
+	if err != nil {
+		log.Fatalf("CLI Reading error: %v", err)
+	}
 }
