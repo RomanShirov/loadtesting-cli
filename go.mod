b1733a624f09a01c2e40e36002b7c35490eff5a2
RomanShirov <RomanShirov.dev@yandex.ru>
2022-11-24 20:35:02 +0300

    Add load testing functionality

--- go.mod
+++ go.mod
@@ -2,9 +2,20 @@ module github.com/RomanShirov/loadtesting-cli
 
 go 1.18
 
-require github.com/manifoldco/promptui v0.9.0
+require (
+	github.com/manifoldco/promptui v0.9.0
+	github.com/tsenart/vegeta v12.7.0+incompatible
+)
 
 require (
+	github.com/bmizerany/perks v0.0.0-20220928223023-dcf613bf3504 // indirect
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
-	golang.org/x/sys v0.0.0-20181122145206-62eef0e2fa9b // indirect
+	github.com/dgryski/go-gk v0.0.0-20200319235926-a69029f61654 // indirect
+	github.com/influxdata/tdigest v0.0.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/streadway/quantile v0.0.0-20220407130108-4246515d968d // indirect
+	golang.org/x/net v0.2.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
 )
--- internal/loadtest/loadtest.go
+++ internal/loadtest/loadtest.go
@@ -1,11 +1,77 @@
 package loadtest
 
-import "time"
+import (
+	"fmt"
+	v "github.com/tsenart/vegeta/lib"
+	"log"
+	"os"
+	"time"
+)
 
 type Config struct {
-	TargetURL                string
+	TargetServiceURL         string
 	RequestMethod            string
 	RequestFrequency         int
 	FrequencyPerUnitDuration time.Duration
 	TestDuration             time.Duration
+}
+
+var (
+	outputDelimiter = "\n------------\n\n"
+)
+
+func debugTarget(service string, method string) v.Target {
+	return v.Target{
+		URL:    service,
+		Method: method,
+	}
+}
+
+func handleReporterError(err error) {
+	if err != nil {
+		log.Fatalf("Reporting error: %v", err)
+	}
+}
+
+func RunLoadTest(c Config) {
+	rate := v.Rate{Freq: c.RequestFrequency, Per: c.FrequencyPerUnitDuration}
+	duration := c.TestDuration
+	targeter := v.NewStaticTargeter(
+		debugTarget(c.TargetServiceURL, c.RequestMethod),
+	)
+
+	attacker := v.NewAttacker()
+	var metrics v.Metrics
+	histogram := v.Histogram{
+		Buckets: []time.Duration{
+			0,
+			10 * time.Millisecond,
+			25 * time.Millisecond,
+			50 * time.Millisecond,
+			100 * time.Millisecond,
+			1000 * time.Millisecond,
+		},
+	}
+
+	fmt.Printf("Starting load test...\n\n")
+	fmt.Printf("\u001B[32mREQUEST_URL: %s \nREQUEST_METHOD: %s \nTEST_DURATION: %s \nREQUEST_FREQUENCY: %d / %s\033[0m \n",
+		c.TargetServiceURL, c.RequestMethod, c.TestDuration, c.RequestFrequency, c.FrequencyPerUnitDuration)
+
+	for res := range attacker.Attack(targeter, rate, duration, "Load Test") {
+		metrics.Add(res)
+		histogram.Add(res)
+	}
+	metrics.Close()
+
+	fmt.Printf(outputDelimiter)
+
+	textReporter := v.NewTextReporter(&metrics)
+	err := textReporter(os.Stdout)
+	handleReporterError(err)
+
+	fmt.Printf(outputDelimiter)
+
+	histogramReporter := v.NewHistogramReporter(&histogram)
+	err = histogramReporter(os.Stdout)
+	handleReporterError(err)
 }
