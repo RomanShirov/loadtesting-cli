f1ddfb0b4bc8ec8afa9f76df6dbae06ac26867bb
RomanShirov <RomanShirov.dev@yandex.ru>
2022-11-25 02:50:59 +0300

    Add CLI interface

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -2,7 +2,10 @@ package cli
 
 import (
 	"fmt"
+	"github.com/RomanShirov/loadtesting-cli/internal/loadtest"
 	"github.com/manifoldco/promptui"
+	"strconv"
+	"time"
 )
 
 func SelectLoadConfigMode() (string, error) {
@@ -19,4 +22,63 @@ func SelectLoadConfigMode() (string, error) {
 	}
 
 	return result, nil
+}
+
+func GatherConfigData() loadtest.Config {
+
+	// Target URL
+	targetPrompt := promptui.Prompt{
+		Label: "Target URL",
+	}
+	targetURL, _ := targetPrompt.Run()
+
+	// Request method
+	reqMethodPrompt := promptui.Select{
+		Label: "Request Method",
+		Items: []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+	}
+	_, requestMethod, _ := reqMethodPrompt.Run()
+
+	// Requests frequency
+	validate := func(input string) error {
+		_, err := strconv.Atoi(input)
+		return err
+	}
+	reqFrequencyPrompt := promptui.Prompt{
+		Label:    "Requests Frequency",
+		Validate: validate,
+	}
+	reqFrequency, _ := reqFrequencyPrompt.Run()
+	reqFrequencyCount, _ := strconv.Atoi(reqFrequency)
+
+	// Request frequency time unit
+	frequencyUnits := map[string]time.Duration{"RPS": time.Second, "RPM": time.Minute}
+	freqTimeUnitPrompt := promptui.Select{
+		Label: "Request per (Second, Minute)?",
+		Items: []string{"RPS", "RPM"},
+	}
+	_, freqTimeUnit, _ := freqTimeUnitPrompt.Run()
+	freqTimeUnitDuration := frequencyUnits[freqTimeUnit]
+
+	// Test duration
+	testDurationValidate := func(input string) error {
+		_, err := strconv.Atoi(input)
+		return err
+	}
+	testDurationPrompt := promptui.Prompt{
+		Label:    "Test Duration (In seconds)",
+		Validate: testDurationValidate,
+	}
+	testDuration, _ := testDurationPrompt.Run()
+	testDurationTime, _ := strconv.Atoi(testDuration)
+
+	config := loadtest.Config{
+		TargetServiceURL:         targetURL,
+		RequestMethod:            requestMethod,
+		RequestFrequency:         reqFrequencyCount,
+		FrequencyPerUnitDuration: freqTimeUnitDuration,
+		TestDuration:             time.Duration(testDurationTime) * time.Second,
+	}
+
+	return config
 }
